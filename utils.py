9a5c5e53e603a7c782cc99d888cde422545ffd00
luizehp <luizehp@al.insper.edu.br>
2023-08-22 08:22:46 -0300

    a

--- utils.py
+++ utils.py
@@ -1,21 +1,26 @@
 import json
+from pathlib import Path
 
 
 def extract_route(req):
-    route = req.split("/n")[0]
-    route = route.split(" ")[1][1::]
-    return route
+    #route = req.split("\n")[0]
+    #print(route)
+    #print("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    #route = route.split(" ")[1][1::]
+    return req.split()[1][1:]
 
 def read_file (path):
     file=open(path,mode="rb")
     return file.read()
 
-def load_data(aq):
-    with open("data/"+aq,mode="r") as file:
-        a = json.load(file)
+def load_data(file):
+    c=Path() / 'data' / file
+    with open(c,"r") as aq:
+        a = json.load(aq)
     return a
 
-def load_template(aq):
-    with open("templates/"+aq,mode="r") as file:
-        a = file.read()
+def load_template(file):
+    c=Path() / 'templates' / file
+    with open(c,"r") as aq:
+        a = aq.read()
     return a
